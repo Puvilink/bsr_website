10d86e718a5e3d4aecd3b00b39986f26e17aed9d
Purushothn22 <purushothn22@gmail.com>
2026-04-06 23:46:33 +0530

    Build fixes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
         services: resolve(__dirname, 'services.html'),
-        tariff: resolve(__dirname, 'tariff.html'),
         contact: resolve(__dirname, 'contact.html'),
       },
     },
